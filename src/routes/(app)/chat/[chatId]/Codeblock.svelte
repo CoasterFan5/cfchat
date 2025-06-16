b0ad74690ac02882862375fd777e1cf5b20d4951
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-06-16 00:16:44 -0400

    Update code block formatting

--- src/routes/(app)/chat/[chatId]/Codeblock.svelte
+++ src/routes/(app)/chat/[chatId]/Codeblock.svelte
@@ -1,8 +1,50 @@
 <script lang="ts">
-	/* eslint-disable svelte/no-at-html-tags */
+	import NoLanguageIcon from '~icons/ph/code';
+	import CopyIcon from '~icons/ph/copy';
 
-	import hljs from 'highlight.js/lib/common';
+	import PythonIcon from '~icons/devicon/python';
+	import JSIcon from '~icons/devicon/javascript';
+	import TypescriptIcon from '~icons/devicon/typescript';
+	import HTMLIcon from '~icons/devicon/html5';
+	import CSSIcon from '~icons/devicon/css3';
+	import SQLIcon from '~icons/devicon/azuresqldatabase';
+	import JavaIcon from '~icons/devicon/java';
+	import CSharpIcon from '~icons/devicon/csharp';
+	import RustIcon from '~icons/devicon/rust';
+
+	import { codeToTokens, bundledLanguages, type BundledLanguage } from 'shiki';
 	import 'highlight.js/styles/atom-one-dark.css';
+	import type { Component } from 'svelte';
+	import type { SVGAttributes } from 'svelte/elements';
+
+	const getSupportedLanguage = (lang?: string | null) => {
+		if (bundledLanguages[lang as BundledLanguage]) {
+			return lang as BundledLanguage;
+		} else {
+			return 'typescript';
+		}
+	};
+
+	type CustomIcon = Component<SVGAttributes<SVGSVGElement>>;
+
+	const languageIconMap: Partial<Record<BundledLanguage, CustomIcon>> = {
+		python: PythonIcon,
+		javascript: JSIcon,
+		typescript: TypescriptIcon,
+		html: HTMLIcon,
+		css: CSSIcon,
+		sql: SQLIcon,
+		java: JavaIcon,
+		rust: RustIcon,
+		csharp: CSharpIcon
+	};
+
+	const getLanguageIcon = (lang: string | undefined) => {
+		if (lang && languageIconMap[lang as BundledLanguage]) {
+			return languageIconMap[lang as BundledLanguage];
+		}
+		return NoLanguageIcon;
+	};
 
 	const {
 		code,
@@ -12,19 +54,98 @@
 		language?: string | null;
 	} = $props();
 
-	let hl = $derived(
-		hljs.highlight(code, {
-			language: language || 'python'
-		}).value
+	let tokensPromise = $derived(
+		codeToTokens(code, {
+			lang: getSupportedLanguage(language),
+			theme: 'vesper'
+		})
 	);
+
+	const copyText = () => {
+		navigator.clipboard.writeText(code);
+	};
 </script>
 
-<pre><code>{@html hl}</code></pre>
+{#await tokensPromise}Loading Code Block{:then tokens}
+	{@const lang = tokens.grammarState?.lang}
+	{@const Icon = getLanguageIcon(lang)}
+	<div class="c" style="background: {tokens.bg}">
+		<div class="toolbar">
+			<span class="language">
+				<Icon />
+				<span class="name">{lang}</span>
+			</span>
+			<div class="tools">
+				<button class="iconButton" onclick={copyText}>
+					<CopyIcon />
+				</button>
+			</div>
+		</div>
+		<pre><code
+				>{#each tokens.tokens as line, index (index)}{#each line as block, index (index)}<span
+							style="color: {block.color}">{block.content}</span
+						>{/each}<br />{/each}</code
+			></pre>
+	</div>
+{/await}
 
 <style lang="scss">
-	pre {
-		background: #23272e;
-		padding: 1rem;
+	.c {
 		border-radius: 0.5rem;
+		white-space: pre-line;
+	}
+
+	.toolbar {
+		background: rgba(0, 0, 0, 0.3);
+		border-radius: 0.5rem 0.5rem 0 0;
+		padding: 0.25rem 0.25rem;
+		padding-left: 1rem;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+	}
+
+	pre {
+		padding: 0.25rem 1rem;
+		margin: 0;
+		padding-bottom: 1rem;
+	}
+
+	.language {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: start;
+		gap: 0.5rem;
+	}
+
+	.tools {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: end;
+	}
+
+	.iconButton {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		aspect-ratio: 1/1;
+		padding: 0.25rem;
+		border-radius: 0.25rem;
+		outline: 0px;
+		border: 0px;
+		font-size: 0.8rem;
+		background: transparent;
+		color: var(--text);
+		cursor: pointer;
+		transition: all cubic-bezier(0.455, 0.03, 0.515, 0.955) 0.1s;
+
+		&:hover {
+			background: var(--primary-10);
+			color: var(--primary);
+		}
 	}
 </style>
